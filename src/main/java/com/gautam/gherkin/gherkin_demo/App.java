d7fd50c59460356759951141826fe0e71795e96e
Gautam Jangra <gautamjangra@cloud.upwork.com>
2017-06-01 16:44:18 +0530

    updated scripts

---
+++ src/main/java/com/gautam/gherkin/gherkin_demo/App.java
@@ -0,0 +1,13 @@
+package com.gautam.gherkin.gherkin_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
